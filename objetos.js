5a6d338748edbd7e179639b0212bf602963c7c59
lukens210 <lukens.210lu@gmail.com>
2025-09-22 08:41:01 -0300

    nem lembro

---
+++ objetos.js
@@ -0,0 +1,15 @@
+
+
+//Criando objetos
+
+let carro = {
+    cor: "Amarelo",
+    marca: "Chevrolet",
+    modelo: "Camaro",
+    ano: 2010,
+    velocidade: 0
+}
+
+console.log(carro.modelo)
+carro.velocidade = 10
+console.log(carro.velocidade)
