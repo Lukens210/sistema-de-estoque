141f1d62ba73e54ed216d74bbaac1fcc7422c0a5
lukens210 <lukens.210lu@gmail.com>
2025-10-02 10:16:10 -0300

    função excluir finalizada e adicionado objeto

--- objetos.js
+++ objetos.js
@@ -7,9 +7,15 @@ let carro = {
     marca: "Chevrolet",
     modelo: "Camaro",
     ano: 2010,
-    velocidade: 0
+    velocidade: 0,
+
+    acelerar: function (valor){
+    this.velocidade += valor
 }
+}
+
 
 console.log(carro.modelo)
 carro.velocidade = 10
+carro.acelerar(50)
 console.log(carro.velocidade)
---
+++ objetos2.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")()
+
+let nome = prompt ("Digite o nome do funcionario: ")
+let cargo = prompt ("Digite o cargo: ")
+let salario = parseFloat(prompt("Digite seu salario"))
